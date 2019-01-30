0722fb8d9a8bc7920191e4858cce4897603a4bde
RukkiesMan <pavelekseev@gmail.com>
2019-01-30 19:26:47 +0300

    Add player's turn function

--- src/bin/game.js
+++ src/bin/game.js
@@ -1,1 +1,9 @@
 import readlineSync from 'readline-sync';
+import { generateRandomNumber } from '../utilities';
+
+const gameObjects = ['rock', 'paper', 'scissors'];
+
+const playersTurn = (choices) => {
+  const indexOfChoice = readlineSync.keyInSelect(choices, 'Your choice?');
+  return choices[indexOfChoice];
+};
