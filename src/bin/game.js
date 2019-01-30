f28f16031e57e46df2599a4b6e04259358b8fbc6
RukkiesMan <pavelekseev@gmail.com>
2019-01-30 22:00:44 +0300

    Rename shapes array

--- src/bin/game.js
+++ src/bin/game.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 import { generateRandomNumber } from '../utilities';
 
-const gameObjects = ['rock', 'paper', 'scissors'];
+const shapes = ['rock', 'paper', 'scissors'];
 
 const playersTurn = (choices) => {
   const indexOfChoice = readlineSync.keyInSelect(choices, 'Your choice?');
@@ -9,3 +9,11 @@ const playersTurn = (choices) => {
 };
 
 const computersTurn = choices => choices[generateRandomNumber(0, choices.length)];
+
+const playRound = (player, computer) => {
+  if (player === computer) {
+    return 0;
+  }
+
+  const 
+};
