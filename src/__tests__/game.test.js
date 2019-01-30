01674dd5cf255d1f00151590e8d4fbbe977fa655
RukkiesMan <pavelekseev@gmail.com>
2019-01-30 19:30:32 +0300

    Rename play round funtion

--- src/__tests__/game.test.js
+++ src/__tests__/game.test.js
@@ -1,17 +1,17 @@
-import { play } from '../bin/game';
+import { playRound } from '../bin/game';
 
 test('Check the condition of the game', () => {
   const playerWin = 1;
   const draw = 0;
   const computerWin = -1;
 
-  expect(play('rock', 'scissors')).toBe(playerWin);
-  expect(play('rock', 'rock')).toBe(draw);
-  expect(play('rock', 'paper')).toBe(computerWin);
-  expect(play('scissors', 'scissors')).toBe(draw);
-  expect(play('scissors', 'rock')).toBe(computerWin);
-  expect(play('scissors', 'paper')).toBe(playerWin);
-  expect(play('paper', 'scissors')).toBe(computerWin);
-  expect(play('paper', 'rock')).toBe(playerWin);
-  expect(play('paper', 'paper')).toBe(draw);
+  expect(playRound('rock', 'scissors')).toBe(playerWin);
+  expect(playRound('rock', 'rock')).toBe(draw);
+  expect(playRound('rock', 'paper')).toBe(computerWin);
+  expect(playRound('scissors', 'scissors')).toBe(draw);
+  expect(playRound('scissors', 'rock')).toBe(computerWin);
+  expect(playRound('scissors', 'paper')).toBe(playerWin);
+  expect(playRound('paper', 'scissors')).toBe(computerWin);
+  expect(playRound('paper', 'rock')).toBe(playerWin);
+  expect(playRound('paper', 'paper')).toBe(draw);
 });
